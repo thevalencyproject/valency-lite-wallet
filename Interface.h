15f1f80475738217e7c5a4569415f3d8f1509704
Isaac Ljubic <134184959+isaacljubic@users.noreply.github.com>
2023-08-18 22:16:43 +1000

    Added Copyright Disclaimer

--- Interface.h
+++ Interface.h
@@ -1,3 +1,6 @@
+/* This file is a part of The Valency Project which is released under the GNU Lesser General Public License v2.1
+   Go to https://github.com/thevalencyproject/valency-lite-wallet/blob/main/LICENSE for further license details.        */
+   
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
